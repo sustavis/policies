637164951304f9bdb444d65789d7395a1498504b
Tony Amende <47224894+tonyamende@users.noreply.github.com>
2024-08-29 21:35:26 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           sidebar: {
             hideable: true,
             sidebarPath: './sidebars.js',
-          }  
+          },  
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
